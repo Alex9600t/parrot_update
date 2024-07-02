6a3df25e9796063396c1f01e495be4f19bf67449
Bduidan <bduidan@Macbook-air-M2---Bduidan.local>
2024-07-02 05:05:32 +0300

    hello

--- parrot_update.sh
+++ parrot_update.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-curl https://raw.githubusercontent.com/alex9600t/parrot_update/master/parrot.sh | bash
+curl -s https://raw.githubusercontent.com/alex9600t/parrot_update/master/parrot.sh | sh
